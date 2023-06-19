1ed46040d1498dadecfc2e36776cb02f431ba936
Dardania <70666490+dardaniaa0@users.noreply.github.com>
2023-06-19 23:34:21 +0200

    hgfh

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
 import 'dotenv/config.js'
 import mongoose from 'mongoose'
-import app from './app.js'
-import MongoClient from './MongoClient.js'
+import app from './src/app.js'
+import MongoClient from './src/MongoClient.js'
 
 async function run() {
   const db = new MongoClient()
   try {
     // Environment variables
-    const PORT = process.env.PORT || 5000
+    const PORT = process.env.PORT || 3003
     // Connect to database, then start the Express server
     await db.connect()
     app.listen(PORT, () => {
